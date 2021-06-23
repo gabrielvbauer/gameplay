a765e225eaeec75873eb594559d42ff25c2a97da
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2021-06-22 21:47:21 -0300

    Screen navigation animation Fix

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,6 +8,8 @@ import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
 import { ListDivider } from "../../components/ListDivider";
 
+import { Background } from '../../components/Background'
+
 import { styles } from "./styles";
 
 export function Home() {
@@ -69,34 +71,36 @@ export function Home() {
   }
 
   return (
-    <View>
-      <View style={styles.header}>
-        <Profile />
-        <ButtonAdd />
-      </View>
+    <Background>
+      <View>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd />
+        </View>
 
-      <CategorySelect 
-        categorySelected={category}
-        setCategory={handleCategorySelect}
-      />
-
-      <View style={styles.content}>
-        <ListHeader
-          title="Partidas agendadas"
-          subtitle="Total 6"
+        <CategorySelect 
+          categorySelected={category}
+          setCategory={handleCategorySelect}
         />
 
-        <FlatList 
-          data={appointments}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <Appointment data={item} />
-          )}
-          ItemSeparatorComponent={() => <ListDivider />}
-          style={styles.matches}
-          showsVerticalScrollIndicator={false}
-        />
+        <View style={styles.content}>
+          <ListHeader
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
+
+          <FlatList 
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Appointment data={item} />
+            )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
       </View>
-    </View>
+    </Background>
   )
 }
