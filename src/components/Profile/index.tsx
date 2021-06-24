826c72c9c5646f2c443af5b98d85a3d6e8bfaf18
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2021-06-24 12:33:01 -0300

    Oauth2 Discord

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { useAuth } from '../../hooks/auth'
 
 import { Avatar } from '../Avatar'
 import { styles } from './styles'
 
 export function Profile() {
+  const { user } = useAuth()
+
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage="https://github.com/gabrielvbauer.png"/>
+      <Avatar urlImage={user.avatar}/>
 
       <View>
         <View style={styles.user}>
@@ -17,11 +20,12 @@ export function Profile() {
           </Text>
 
           <Text style={styles.username}>
-            Gabriel  
+            { user.firstName }
           </Text>       
         </View>
 
-        <Text style={styles.message}>
+        {/* TODO: Gerar frases aleatórias */}
+        <Text style={styles.message}> 
           Hoje é dia de vitória
         </Text>
       </View>
--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -3,8 +3,9 @@ import {
   View,
   Text,
   Image,
+  Alert,
+  ActivityIndicator
 } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
 
 import { useAuth } from '../../hooks/auth'
 
@@ -13,14 +14,17 @@ import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 import { Background } from '../../components/Background'
+import { theme } from '../../global/styles/theme'
 
 export function SignIn() {
-  const navigation = useNavigation();
+  const { loading, signIn } = useAuth();
 
-  const { user } = useAuth();
-
-  function handleSignIn(){
-    navigation.navigate('Home');
+  async function handleSignIn(){
+    try {
+      await signIn()
+    } catch (error) {
+      Alert.alert(error)
+    }
   }
 
   return (
@@ -44,10 +48,14 @@ export function SignIn() {
             favoritos com seus amigos
           </Text>
 
-          <ButtonIcon 
-            title="Entrar com Discord"
-            onPress={handleSignIn}
-          />
+          {
+            loading 
+            ? <ActivityIndicator color={theme.colors.primary}/>
+            : <ButtonIcon 
+                title="Entrar com Discord"
+                onPress={handleSignIn}
+              />
+          }
         </View>
       </View>
     </Background>
