592fb06f49edb21653b7b769cb8776df7644b843
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2021-06-24 10:57:38 -0300

    User context creation

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -6,6 +6,8 @@ import {
 } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
+import { useAuth } from '../../hooks/auth'
+
 import { ButtonIcon } from '../../components/ButtonIcon'
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
@@ -14,6 +16,8 @@ import { Background } from '../../components/Background'
 
 export function SignIn() {
   const navigation = useNavigation();
+
+  const { user } = useAuth();
 
   function handleSignIn(){
     navigation.navigate('Home');
