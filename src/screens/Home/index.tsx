a73847acecda7a82e6c01311feedf95351510863
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2021-06-25 10:32:11 -0300

    Appointments quantity

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -69,7 +69,7 @@ export function Home() {
           <>
             <ListHeader
               title="Partidas agendadas"
-              subtitle="Total 6"
+              subtitle={`Total ${appointments.length}`}
             />
 
             <FlatList 
